26c570802e87d302ee8e7508ae36d4e74dd83496
Meletis <melnikolaidis86@gmail.com>
2019-11-05 01:26:37 +0200

    Initial commit of Instagram Widget Extension

---
+++ app/code/Elegento/Instagram/Block/Widget/Instagram.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Elegento\Instagram\Block\Widget;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class Instagram extends Template implements BlockInterface
+{
+    protected $_template = "widget/instagram.phtml";
+
+}
