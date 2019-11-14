6893352c7338a9af7473b36c36ed517ae27d9fd7
Meletis <melnikolaidis86@gmail.com>
2019-11-15 00:14:12 +0200

    Render saved images instead of instagram on frontend

--- app/code/Elegento/Instagram/Block/Widget/Instagram.php
+++ app/code/Elegento/Instagram/Block/Widget/Instagram.php
@@ -16,41 +16,40 @@ class Instagram extends Template implements BlockInterface
     protected $instagramHelper;
 
     /**
+     * @var \Elegento\Instagram\Model\ResourceModel\Instagram\Collection
+     */
+    protected $instagramCollectionFactory;
+
+    /**
      * Instagram constructor.
      * @param Template\Context $context
+     * @param \Elegento\Instagram\Model\ResourceModel\Instagram\CollectionFactory $instagramCollectionFactory
      * @param \Elegento\Instagram\Helper\Instagram $instagramHelper
      * @param array $data
      */
     public function __construct(
         \Magento\Framework\View\Element\Template\Context $context,
+        \Elegento\Instagram\Model\ResourceModel\Instagram\CollectionFactory $instagramCollectionFactory,
         \Elegento\Instagram\Helper\Instagram $instagramHelper,
         array $data = []
     ){
         parent::__construct($context, $data);
+        $this->instagramCollectionFactory = $instagramCollectionFactory;
         $this->instagramHelper = $instagramHelper;
         $this->_instagramData =  $this->instagramHelper->getLatestMedia();
     }
 
     /**
-     * @return array
+     * @return \Elegento\Instagram\Model\ResourceModel\Instagram\Collection
      */
-    public function getInstagramData()
+    public function getInstagramCollection()
     {
-        $data = array();
-        foreach ($this->_instagramData as $post) {
-            $data[$post['id']]['image_src'] = $post['images']['low_resolution']['url'];
-            $data[$post['id']]['link'] = $post['link'];
-            $data[$post['id']]['caption'] = $post['caption']['text'];
-        }
+        /** @var \Elegento\Instagram\Model\ResourceModel\Instagram\Collection $instagramCollection */
+        $instagramCollection = $this->instagramCollectionFactory->create();
 
-        return $data;
-    }
+        $instagramCollection->setPageSize(\Elegento\Instagram\Helper\Instagram::IMAGE_COUNT) // only get images that is going to be displayed
+            ->setCurPage(1);
 
-    /**
-     * @return string
-     */
-    public function getAccountName()
-    {
-        return $this->instagramHelper->getAccountName();
+        return $instagramCollection;
     }
 }
